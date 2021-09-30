4c6d8208f24c5055083f7a683eaad27e164143d1
dgoceva <44052125+dgoceva@users.noreply.github.com>
2021-09-30 10:49:25 +0300

    init project

---
+++ hello.py
@@ -0,0 +1,6 @@
+# print('Hello Python!')
+# print(3/2)
+
+a = int(input('a='))
+a = 5
+print('S='+str(a*a))
---
+++ square.py
@@ -0,0 +1,11 @@
+a = 5
+# a = int(input('a='))
+# a = float(input('a = '))
+# x = input('a=')
+# a = float(x)
+# print('S='+str(a*a))
+# b = float(input('b = '))
+# print('S rectangle = '+str(a*b))
+# s = a*b
+# print('S rect = '+str(s))
+print(2+2*2)
