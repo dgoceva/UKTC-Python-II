cdae57e2b0040843bd263c493279634d60078695
dgoceva <44052125+dgoceva@users.noreply.github.com>
2021-09-30 11:40:36 +0300

    numbers

--- hello.py
+++ hello.py
@@ -1,6 +1,19 @@
 # print('Hello Python!')
 # print(3/2)
 
-a = int(input('a='))
-a = 5
-print('S='+str(a*a))
+# a = int(input('a='))
+# a = 5
+# print('S='+str(a*a))
+
+# num1, num2 = map(float, input('Input numbers: ').split(','))
+# print(num1, num2, num1+num2, sep='#', end='\n\n')
+
+# name = input()
+# print('Hello ', name, '!')
+
+first_name = input('Name: ')
+last_name = input('Family: ')
+age = int(input('Age: '))
+town = input('Town: ')
+text = '{0}\n{1}\n{2} years old\nIvantown {3}.'
+print(text.format(first_name, last_name, age, town))
