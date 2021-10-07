c1ba599943b4e68654ccfa92c9c727fa604279da
dgoceva <44052125+dgoceva@users.noreply.github.com>
2021-10-07 11:32:08 +0300

    square

--- printf_formats.py
+++ printf_formats.py
@@ -1,14 +1,22 @@
-a = int(input('a= '))
-b = int(input('b= '))
-p = 2*a + 2*b
+# a = int(input('a= '))
+# b = int(input('b= '))
+# p = 2*a + 2*b
 
-print('P = ', p)
-print('P = '+str(p))
-print('P = %5d' % (p))
-print('P = {0}'.format(p))
-print(f'P = {p}')
+# print('P = ', p)
+# print('P = '+str(p))
+# print('P = %5d' % (p))
+# print('P = {0}'.format(p))
+# print(f'P = {p}')
 
-print('price = %.2f\nVAT = %.3f' % (1.6, 1.6*0.2))
+# print('price = %.2f\nVAT = %.3f' % (1.6, 1.6*0.2))
 
-print('(3+5)*(4-2) = '+str((3+5)*(4-2)))
-print('3+5*4-2 = '+str(3+5*4-2))
+# print('(3+5)*(4-2) = '+str((3+5)*(4-2)))
+# print('3+5*4-2 = '+str(3+5*4-2))
+
+a = float(input('a = '))
+b = float(input('b = '))
+h = float(input('h = '))
+s = (a+b)/2*h
+print(f'S = {s}')
+print('S = %.1f' % (s))
+print('S = ', round(s, 1))
