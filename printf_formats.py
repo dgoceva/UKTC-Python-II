7656e871b09f4a404d12d246fda45056d5730448
dgoceva <44052125+dgoceva@users.noreply.github.com>
2021-10-07 11:23:03 +0300

    print&format

---
+++ printf_formats.py
@@ -0,0 +1,14 @@
+a = int(input('a= '))
+b = int(input('b= '))
+p = 2*a + 2*b
+
+print('P = ', p)
+print('P = '+str(p))
+print('P = %5d' % (p))
+print('P = {0}'.format(p))
+print(f'P = {p}')
+
+print('price = %.2f\nVAT = %.3f' % (1.6, 1.6*0.2))
+
+print('(3+5)*(4-2) = '+str((3+5)*(4-2)))
+print('3+5*4-2 = '+str(3+5*4-2))
