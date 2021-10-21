7bd59dfe3c079a6572c4e905e91623757e9788f3
Daniela Gotseva <dgoceva@tu-sofia.bg>
2021-10-21 11:21:09 +0300

    final version - 21-10-21

--- money_converter.py
+++ money_converter.py
@@ -1,3 +1,24 @@
-a = float(input('USD = '))
-b = a*1.79549
-print('{0} USD to BGN = '.format(a), round(b, 2))
+amount = float(input('Amount = '))
+money = input('From = ')
+out_money = input('To = ')
+
+result = amount
+if money == 'USD':
+    result *= 1.79549  # result = result * 1.79549
+elif money == 'EUR':
+    result *= 1.95583
+elif money == 'GBP':
+    result *= 2.53405
+elif money != 'BGN':
+    print('Invalid money')
+
+if out_money == 'USD':
+    result /= 1.79549  # result = result * 1.79549
+elif out_money == 'EUR':
+    result /= 1.95583
+elif out_money == 'GBP':
+    result /= 2.53405
+elif out_money != 'BGN':
+    print('Invalid money')
+
+print('{0} {1} = {2} {3}'.format(amount, money, round(result, 2), out_money))
