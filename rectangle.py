ca11da16468feda7b5de930775399dd8661e081c
Daniela Gotseva <dgoceva@tu-sofia.bg>
2021-10-21 11:02:41 +0300

    rectangle ended

--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,13 @@
-a = 30
-print('a=30')
-b = 50
-print('b=50')
+x1 = int(input('x1 = '))
+y1 = int(input('y1 = '))
+x2 = int(input('x2 = '))
+y2 = int(input('y2 = '))
+
+a = max(x1, x2)-min(x1, x2)
+b = max(y1, y2)-min(y1, y2)
+print('a =', a)
+print('b =', b)
+
 S = (a*b)
 P = 2*(a+b)
 
