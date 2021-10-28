09be67f3fcc017ee85760730ff8f4e0175a34fce
Daniela Gotseva <dgoceva@tu-sofia.bg>
2021-10-28 11:09:05 +0300

    strptime added

--- date1000.py
+++ date1000.py
@@ -1,11 +1,7 @@
 import datetime as dt
 
-day = int(input('[01-31] '))
-month = int(input('[01-12] '))
-year = int(input('year: '))
-
-input_date = dt.datetime(year, month, day)
-print(input_date.strftime('%d-%m-%Y'))
+str_date = input('Input date[dd-mm-yyyy]: ')
+input_date = dt.datetime.strptime(str_date, '%d-%m-%Y')
 
 result = input_date+dt.timedelta(days=1000)
 print(result.strftime('%d-%m-%Y'))
